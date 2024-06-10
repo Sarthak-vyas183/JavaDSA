178fa73a6669b444faa65e10f3d9e76b037e9232
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-06-10 09:00:44 +0530

    selectionSort

--- selectionSort.java
+++ selectionSort.java
@@ -1,28 +1,23 @@
 public class selectionSort {
     public static void selection(int arr[]) {
-        int smallest = 0;
+        for(int i=0; i<arr.length-1; i++) {
+            int minpos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minpos] > arr[j]){
+                    minpos = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minpos];
+            arr[minpos] = temp;
+        }
         for(int i=0; i<arr.length; i++) {
-            
-              for(int j=i; j<arr.length; j++) {
-                  if(arr[j] < arr[smallest]) {
-                    smallest = j;
-                  }
-                  int temp = arr[smallest];
-                  arr[smallest] = arr[0];
-                  arr[0] = temp;
-                 smallest = 0;
-              }
-             
-
-        } 
-        System.out.println(arr[smallest]);
-
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
-        int arr[] = {9,5,4,0,3};
-         selection(arr);
+        int arr[] = {9,5,4,0,3,5,6,1,7};
+
+        selection(arr);
     }
 }
